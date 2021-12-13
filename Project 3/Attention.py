f98e0102034172c92f48ffb9bc6b223b6c826cb8
TheUnity42 <jtalonh@gmail.com>
2021-12-13 12:37:58 -0500

    Project Woproject workrk

--- Project 3/Attention.py
+++ Project 3/Attention.py
@@ -41,6 +41,44 @@ class AttentionUtils:
 
         return output, attention_weights
 
+    def offsetElu(x):
+        return tf.nn.elu(x) + 1
+
+    def build_linear_attention(feature_map=None):
+        if feature_map is None:
+            feature_map = AttentionUtils.offsetElu
+
+        def lin_attention(q, k, v, mask=None):
+            """Calculate the attention weights.
+                q, k, v must have matching leading dimensions.
+                k, v must have matching penultimate dimension, i.e.: seq_len_k = seq_len_v.
+                The mask has different shapes depending on its type(padding or look ahead)
+                but it must be broadcastable for addition.
+
+                Args:
+                    q: query shape == (..., seq_len_q, depth)
+                    k: key shape == (..., seq_len_k, depth)
+                    v: value shape == (..., seq_len_v, depth_v)
+                    mask: Float tensor with shape broadcastable
+                        to (..., seq_len_q, seq_len_k). Defaults to None.
+
+                Returns:
+                    output, attention_weights
+                """
+
+            fQ = feature_map(q)
+            fK = feature_map(k)
+
+            Z = 1/tf.einsum('bhsi,bhsi->bhsi', q, tf.cumsum(k, axis=1))
+            S = tf.cumsum(tf.einsum('bhsi,bhsi->bhsi', q, v), axis=1)
+
+            Vp = tf.multiply(q, S)
+            Vp = tf.realdiv(Vp, Z)
+
+            return Vp, None
+        return lin_attention
+
+
     def get_angles(pos, i, d_model):
         angle_rates = 1 / np.power(10000, (2 * (i//2)) / np.float32(d_model))
         return pos * angle_rates
@@ -138,12 +176,13 @@ class AttentionUtils:
             label='LR')
         plt.legend()
 
-    def mask_loss(loss_func):
+    def mask_loss(loss_func, mask_tok):
         """
         Mask the loss function to ignore padding tokens
         """
         def masked_loss(y_true, y_pred):
-            mask = tf.cast(tf.math.not_equal(y_true, 0), tf.float32)
+            mask = tf.math.not_equal(y_true, mask_tok)
+            mask = tf.cast(mask, tf.float32)
 
             loss = loss_func(y_true, y_pred)
             loss *= mask
@@ -151,27 +190,29 @@ class AttentionUtils:
 
         return masked_loss
 
-    def mask_accuracy(acc_func):
+    def mask_accuracy(acc_func, mask_tok):
         """
         Mask the accuracy function to ignore padding tokens
         """
         def m_acc(y_true, y_pred):
-            mask = tf.cast(tf.math.not_equal(y_true, 0), tf.float32)
+            mask = tf.cast(tf.math.not_equal(y_true, mask_tok), tf.float32)
             print(y_true.shape, y_pred.shape)
             acc = acc_func(y_true, y_pred)
             acc *= mask
             return tf.reduce_sum(acc) / tf.reduce_sum(mask)
         return m_acc
 
-    def masked_accuracy(real, pred):
-        accuracies = tf.equal(real, tf.argmax(pred, axis=2))
+    def get_masked_acc(mask_tok):
+        def masked_accuracy(real, pred):
+            accuracies = tf.equal(real, tf.argmax(pred, axis=2, output_type=tf.int32))
+            mask = tf.math.not_equal(real, mask_tok)
 
-        mask = tf.math.logical_not(tf.math.equal(real, 0))
-        accuracies = tf.math.logical_and(mask, accuracies)
+            accuracies = tf.math.logical_and(mask, accuracies)
 
-        accuracies = tf.cast(accuracies, dtype=tf.float32)
-        mask = tf.cast(mask, dtype=tf.float32)
-        return tf.reduce_sum(accuracies)/tf.reduce_sum(mask)
+            accuracies = tf.cast(accuracies, dtype=tf.float32)
+            mask = tf.cast(mask, dtype=tf.float32)
+            return tf.reduce_sum(accuracies)/tf.reduce_sum(mask)
+        return masked_accuracy
 
     def tokenize(sentence, token_dict):
         """
@@ -190,10 +231,14 @@ class AttentionUtils:
 
 
 class MultiHeadAttention(tf.keras.layers.Layer):
-    def __init__(self, d_model, num_heads):
+    def __init__(self, d_model, num_heads, attn_func=None):
         super(MultiHeadAttention, self).__init__()
         self.num_heads = num_heads
         self.d_model = d_model
+        if attn_func is None:
+            self.attn_func = AttentionUtils.scaled_dot_product_attention
+        else:
+            self.attn_func = attn_func
 
         assert d_model % self.num_heads == 0
 
@@ -228,8 +273,7 @@ class MultiHeadAttention(tf.keras.layers.Layer):
 
         # scaled_attention.shape == (batch_size, num_heads, seq_len_q, depth)
         # attention_weights.shape == (batch_size, num_heads, seq_len_q, seq_len_k)
-        scaled_attention, attention_weights = AttentionUtils.scaled_dot_product_attention(
-            q, k, v, mask)
+        scaled_attention, attention_weights = self.attn_func(q, k, v, mask)
 
         # (batch_size, seq_len_q, num_heads, depth)
         scaled_attention = tf.transpose(scaled_attention, perm=[0, 2, 1, 3])
@@ -244,10 +288,10 @@ class MultiHeadAttention(tf.keras.layers.Layer):
 
 
 class EncoderLayer(tf.keras.layers.Layer):
-    def __init__(self, d_model, num_heads, dff, rate=0.1):
+    def __init__(self, d_model, num_heads, dff, rate=0.1, attn_func=None):
         super(EncoderLayer, self).__init__()
 
-        self.mha = MultiHeadAttention(d_model, num_heads)
+        self.mha = MultiHeadAttention(d_model, num_heads, attn_func)
         self.ffn = AttentionUtils.pointwise_feed_forward(d_model, dff)
 
         self.layernorm1 = tf.keras.layers.LayerNormalization(epsilon=1e-6)
@@ -273,11 +317,11 @@ class EncoderLayer(tf.keras.layers.Layer):
 
 
 class DecoderLayer(tf.keras.layers.Layer):
-  def __init__(self, d_model, num_heads, dff, rate=0.1):
+  def __init__(self, d_model, num_heads, dff, rate=0.1, attn_func=None):
     super(DecoderLayer, self).__init__()
 
-    self.mha1 = MultiHeadAttention(d_model, num_heads)
-    self.mha2 = MultiHeadAttention(d_model, num_heads)
+    self.mha1 = MultiHeadAttention(d_model, num_heads, attn_func)
+    self.mha2 = MultiHeadAttention(d_model, num_heads, attn_func)
 
     self.ffn = AttentionUtils.pointwise_feed_forward(d_model, dff)
 
@@ -314,7 +358,7 @@ class DecoderLayer(tf.keras.layers.Layer):
 
 class Encoder(tf.keras.layers.Layer):
   def __init__(self, num_layers, d_model, num_heads, dff, input_vocab_size,
-               maximum_position_encoding, embedding_weights=None, freeze_embedding=False, rate=0.1):
+               maximum_position_encoding, embedding_weights=None, freeze_embedding=False, rate=0.1, attn_func=None):
     super(Encoder, self).__init__()
 
     self.d_model = d_model
@@ -324,7 +368,7 @@ class Encoder(tf.keras.layers.Layer):
     self.pos_encoding = AttentionUtils.positional_encoding(maximum_position_encoding,
                                                            self.d_model)
 
-    self.enc_layers = [EncoderLayer(d_model, num_heads, dff, rate)
+    self.enc_layers = [EncoderLayer(d_model, num_heads, dff, rate, attn_func)
                        for _ in range(num_layers)]
 
     self.dropout = tf.keras.layers.Dropout(rate)
@@ -348,7 +392,7 @@ class Encoder(tf.keras.layers.Layer):
 
 class Decoder(tf.keras.layers.Layer):
   def __init__(self, num_layers, d_model, num_heads, dff, target_vocab_size,
-               maximum_position_encoding, rate=0.1):
+               maximum_position_encoding, rate=0.1, attn_func=None):
     super(Decoder, self).__init__()
 
     self.d_model = d_model
@@ -358,7 +402,7 @@ class Decoder(tf.keras.layers.Layer):
     self.pos_encoding = AttentionUtils.positional_encoding(
         maximum_position_encoding, d_model)
 
-    self.dec_layers = [DecoderLayer(d_model, num_heads, dff, rate)
+    self.dec_layers = [DecoderLayer(d_model, num_heads, dff, rate, attn_func)
                        for _ in range(num_layers)]
     self.dropout = tf.keras.layers.Dropout(rate)
 
@@ -387,13 +431,23 @@ class Decoder(tf.keras.layers.Layer):
 
 class Transformer(tf.keras.Model):
   def __init__(self, num_layers, d_model, num_heads, dff, input_vocab_size,
-               target_vocab_size, pe_input, pe_target, rate=0.1, pad_token=0):
+               target_vocab_size, pe_input, pe_target, rate=0.1, pad_token=0, attn_func=None):
     super().__init__()
+
+    if type(attn_func) == list or type(attn_func) == tuple:
+        assert len(attn_func) == 2
+        self.encoder_attn_func = attn_func[0]
+        self.decoder_attn_func = attn_func[1]
+    else:
+        self.encoder_attn_func = attn_func
+        self.decoder_attn_func = attn_func
+
+
     self.encoder = Encoder(num_layers, d_model, num_heads, dff,
-                           input_vocab_size, pe_input, rate)
+                           input_vocab_size, pe_input, rate, self.encoder_attn_func)
 
     self.decoder = Decoder(num_layers, d_model, num_heads, dff,
-                           target_vocab_size, pe_target, rate)
+                           target_vocab_size, pe_target, rate, self.decoder_attn_func)
 
     self.final_layer = tf.keras.layers.Dense(target_vocab_size)
 
@@ -438,6 +492,45 @@ class Transformer(tf.keras.Model):
 
     return enc_padding_mask, look_ahead_mask, dec_padding_mask
 
+class EncoderModel(tf.keras.Model):
+    def __init__(self, num_layers, d_model, num_heads, dff, input_vocab_size,
+               target_vocab_size, pe_input, pe_target, rate=0.1, pad_token=0, attn_func=None):
+        super().__init__()
+
+        if type(attn_func) == list or type(attn_func) == tuple:
+            assert len(attn_func) == 2
+            self.encoder_attn_func = attn_func[0]
+            self.decoder_attn_func = attn_func[1]
+        else:
+            self.encoder_attn_func = attn_func
+            self.decoder_attn_func = attn_func
+
+        self.encoder = Encoder(num_layers, d_model, num_heads, dff,
+                            input_vocab_size, pe_input, rate, self.encoder_attn_func)
+
+        self.final_layer = tf.keras.layers.Dense(target_vocab_size)
+
+        self.pad_token = pad_token
+
+    def call(self, inputs, training):
+        # Keras models prefer if you pass all your inputs in the first argument
+        inp = inputs
+
+        enc_padding_mask = self.create_masks(inp)
+
+        # (batch_size, inp_seq_len, d_model)
+        enc_output = self.encoder(inp, training, enc_padding_mask)        
+
+        # (batch_size, tar_seq_len, target_vocab_size)
+        final_output = self.final_layer(enc_output)
+
+        return final_output
+
+    def create_masks(self, inp):
+        # Encoder padding mask
+        enc_padding_mask = AttentionUtils.create_look_ahead_mask(inp.shape[1])
+
+        return enc_padding_mask
 
 class Fastformer_MultiHeadAttention(tf.keras.layers.Layer):
     def __init__(self, d_model, num_heads, max_seq_len):
